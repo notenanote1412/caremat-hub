d356fbcf7c9d5a7a17c6c9f5783f2cd737c299e7
Aphiwat Chawilai <wat.chawilai@gmail.com>
2022-09-23 09:30:32 +0700

    title_timeslot align left

--- resources/views/time_select.blade.php
+++ resources/views/time_select.blade.php
@@ -10,11 +10,11 @@
                 <input id="reference" type="hidden" value="{{ $reference }}" placeholder="">
                 @csrf
                 <template v-for="slot_date in lists.date_lists">
-                    <h6 class="title_timeslot text-black" v-text="date_label_thai(slot_date)"></h6>
                     <div class="timeslot mb-2">
+                        <h6 class="title_timeslot text-black" v-text="date_label_thai(slot_date)"></h6>
                         <template v-for="(slot, index) in slot_arr"
                             v-if="hide_slot_pass(slot) && show_slot_each_day(slot_date, slot)">
-                                <input class="checkbox-budget" type="radio" name="budget" :id="`slot_${index + 1}`" @change="time_slot_checked(slot)">
+                            <input class="checkbox-budget" type="radio" name="budget" :id="`slot_${index + 1}`" @change="time_slot_checked(slot)">
                             <label class="for-checkbox-budget" :for="`slot_${index + 1}`">
                                 <span :data-hover="show_time_slot(slot)" v-text="show_time_slot(slot)"></span>
                             </label>
@@ -40,4 +40,12 @@
 
     <script src="{{url('assets/js/owl.carousel.js')}}"></script>
 
+@endsection
+
+@section('style')
+    <style>
+        .title_timeslot{
+            text-align: left;
+        }
+    </style>
 @endsection
